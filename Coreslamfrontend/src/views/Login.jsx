1d6bdb413becac56a56a516d478edec1e8ad05f4
Fernandodennis <Fernandodennis038@gmail.com>
2025-05-06 09:40:27 +0700

    login form updated

--- Coreslamfrontend/src/views/Login.jsx
+++ Coreslamfrontend/src/views/Login.jsx
@@ -1,9 +1,33 @@
 import { Link } from "react-router-dom"
+import { useRef, useState } from "react"
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Login() {
 
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [errors, setErrors] = useState(null)
+    const {setUser, setToken} = useStateContext()
+
     const onSubmit = (ev) => {
         ev.preventDefault()
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        axiosClient.post('/signup', payload)
+        .then(({data}) => {
+            setUser(data.user)
+            setToken(data.token)
+        })
+        .catch(err => {
+            console.log(err)
+            const response = err.response;
+            if (response && response.status === 422) { 
+                setErrors(response.data.errors);
+            }
+        })
     }
 
     return (
@@ -13,8 +37,14 @@ export default function Login() {
                     <h1 className="title">
                         Login
                     </h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    {errors && <div className="alert">
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                    }
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
                     <button className="btn btn-block">Login</button>
                     <p className="message">
                         Not Registered? <Link to="/signup">Create an account</Link>
