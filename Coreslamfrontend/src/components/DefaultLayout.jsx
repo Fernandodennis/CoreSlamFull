c6d03faf7c5673914950097d3775972e36b13941
Fernandodennis <Fernandodennis038@gmail.com>
2025-05-14 14:16:13 +0700

    signup, login, logout is possible now

--- Coreslamfrontend/src/components/DefaultLayout.jsx
+++ Coreslamfrontend/src/components/DefaultLayout.jsx
@@ -1,9 +1,11 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
 
-    const {user, token} = useStateContext()
+    const {user, token, setUser, setToken} = useStateContext()
 
     if (!token) {
         return <Navigate to="/login" />
@@ -11,7 +13,20 @@ export default function DefaultLayout() {
 
     const onLogout = (ev) => {
         ev.preventDefault()
+
+        axiosClient.post('logout')
+        .then(() => {
+            setUser({})
+            setToken(null)
+        })
     }
+
+    useEffect(() => {
+        axiosClient.get('/user')
+        .then(({data}) => {
+            setUser(data)
+        })
+    }, [])
 
     return (
         <div id="defaultLayout">
@@ -25,7 +40,7 @@ export default function DefaultLayout() {
                     Header
                 </div>
                 <div>
-                    {user.name}
+                    {user.username}
                     <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
                 </div>
             </header>
